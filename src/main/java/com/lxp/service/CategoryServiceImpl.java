5edcb0a3763114de8e5319e1343d72ebcfd637d6
dkstpwns10 <dkstpwns10@naver.com>
2025-10-17 16:08:38 +0900

    refactor/[Category] controller생성 및 예외처ë¦/ë메인 병합

--- src/main/java/com/lxp/service/CategoryServiceImpl.java
+++ src/main/java/com/lxp/service/CategoryServiceImpl.java
@@ -1,25 +1,22 @@
 package com.lxp.service;
 
-import static com.lxp.util.Validator.selectValidator;
-
 import com.lxp.dao.CategoryDAO;
 import com.lxp.model.Category;
 import com.lxp.util.InputUtil;
-import com.lxp.util.Logging;
+import com.lxp.util.Validator;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CategoryServiceImpl implements CategoryService {
-    private final Logging logger;
     private final CategoryDAO categoryDAO;
-    private final Scanner sc =  new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
 
     //테스트용 생성자
     public CategoryServiceImpl(CategoryDAO categoryDAO) {
         this.categoryDAO = categoryDAO;
-        this.logger = new Logging(CategoryServiceImpl.class);
     }
 
 
@@ -28,84 +25,71 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public List<Category> getAllCategories() throws SQLException {
-        List<Category> categories = categoryDAO.getAllCategory();
-        logger.debug("[CategoryService] Categories : {}", categories);
-
-        if (!categories.isEmpty()) {
+    public List<Category> getAllCategories() throws SQLException, RuntimeException {
+        List<Category> categories = new ArrayList<>();
+        try {
+            categories = categoryDAO.getAllCategory();
+            if (categories.isEmpty()) {
+                throw new RuntimeException("존재하는 카테고리가 없습니다.");
+            }
             for (int i = 0; i < categories.size(); i++) {
                 System.out.printf("[%d] %s\n", i + 1, categories.get(i).getCategory_name());
             }
-            return categories;
+        } catch (SQLException e) {
+            throw new SQLException("카테고리를 가져오는 도중 오류가 발생했습니다.");
         }
-
-        System.out.println("존재하는 카테고리가 없습니다.");
-
         return categories;
     }
 
     @Override
-    public Category selectCategory() throws SQLException {
-        logger.info("[CategoryService] selected category 시작");
+    public Category selectCategory() throws SQLException, RuntimeException {
         List<Category> categories = getAllCategories();
+        int select;
+        int categorySize = categories.size();
+        boolean check;
 
-        int categorySize = categories.size() ;
-
-        int select = selectValidator(categorySize);
-
-        Category selectedCategory = categories.get(select - 1);
-        logger.debug("[CategoryService] Selected Category : {}", selectedCategory);
-
-        return selectedCategory;
-    }
-    @Override
-    public void saveCategory() throws SQLException {
-        logger.info("[CategoryService] Save Category 시작");
-        Category savedCategory = null;
         do {
-            String categoryName = InputUtil.readString("추가할 카테고리 이름을 입력하세요: ");
-            try {
-                savedCategory = Category.forNewCreate(categoryName);
-            } catch (IllegalArgumentException e) {
-                System.err.println("⚠️ 오류: " + e.getMessage());
+            select = InputUtil.readValidInt();
+            check = Validator.selectValidator(select, categorySize);
+        } while (check);
+
+        return categories.get(select - 1);
+    }
+
+    @Override
+    public void saveCategory(String categoryName) throws IllegalArgumentException, SQLException {
+        Category savedCategory;
+        do {
+            savedCategory = Category.forNewCreate(categoryName);
+            Long affectRow = categoryDAO.saveCategory(savedCategory);
+            if (affectRow > 0) {
+                System.out.printf("[%s]카테고리가 생성되었습니다.", savedCategory.getCategory_name());
             }
         } while (savedCategory == null);
-
-        logger.debug("[CategoryService] Save Category : {}", savedCategory);
-        categoryDAO.saveCategory(savedCategory);
     }
 
     @Override
-    public void deleteCategory() throws SQLException {
-        logger.info("[CategoryService] Delete Category 시작");
-        System.out.println("삭제할 카테고리를 선택해 주세요.");
+    public void deleteCategory() throws SQLException, RuntimeException {
         Category deletedCategory = selectCategory();
-        logger.debug("[CategoryService] Delete Category : {}", deletedCategory);
         categoryDAO.deleteCategoryById(deletedCategory);
     }
 
     @Override
-    public void updateCategoryName() throws SQLException {
-        logger.info("[CategoryService] Update Category 시작");
-        System.out.println("수정할 카테고리를 선택해 주세요.");
+    public void updateCategoryName() throws SQLException, IllegalArgumentException {
         Category tmp = selectCategory();
 
-        if (tmp == null) {
-            System.out.println("수정할 카테고리가 없습니다.");
-            return;
-        }
         boolean isUpdateSuccessful = false;
         do {
             String updateCategoryName = InputUtil.readString("변경할 이름을 입력해주세요");
 
             Category updatedCategory = Category.forUpdate(tmp.getCategory_id(), updateCategoryName);
-            logger.debug("[CategoryService] Updated Category : {}", updatedCategory);
             try {
                 categoryDAO.updateCategoryName(updatedCategory);
-                System.out.printf("[%s]의 이름이 [%s]로 변경되었습니다.\n", tmp.getCategory_name(), updatedCategory.getCategory_name());
+                System.out.printf("[%s]의 이름이 [%s]로 변경되었습니다.\n", tmp.getCategory_name(),
+                        updatedCategory.getCategory_name());
 
                 isUpdateSuccessful = true;
-            } catch (IllegalArgumentException e) {
+            } catch (IllegalArgumentException | SQLException e) {
                 System.out.println(e.getMessage());
             }
         } while (!isUpdateSuccessful);
